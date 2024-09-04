bda9b815c5b8d68cce9c1db71bb5227641946122
zlove77z <zlove77z@naver.com>
2024-09-04 12:40:31 +0900

    20240904_1240 Test3.java update3

--- src/t50_Exercise/T3.java
+++ src/t50_Exercise/T3.java
@@ -2,9 +2,12 @@ package t50_Exercise;
 
 public class T3 {
 	public static void main(String[] args) {
-		System.out.println("1~10까지 출력하기");
+		int tot=0;
+		System.out.println("**** 1~10까지의 합을 출력하는 프로그램 ****");
 		for(int i=1; i<=10; i++) {
 			System.out.println("i : " + i);
+			tot += i;
 		}
+		System.out.println("1~10까지의 합은: " +tot);
 	}
 }
