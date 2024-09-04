aebc15857d84f751b0209799f1503827c1b42b3c
green <green@DESKTOP-633ACC1>
2024-09-04 14:35:55 +0900

    20240904_1435 T4 update

--- src/t50_Exercise/T4.java
+++ src/t50_Exercise/T4.java
@@ -3,7 +3,8 @@ package t50_Exercise;
 public class T4 {
 
 	public static void main(String[] args) {
-		System.out.println("안녕하세요.!!!");
+		System.out.println("안녕하세요.!!!!!!!");
+		System.out.println("  ");
 	}
 	
 	
